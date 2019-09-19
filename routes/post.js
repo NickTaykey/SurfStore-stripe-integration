43ad2d8bd795a8ce36092e053fdb94143053a832
Nick Tk <nico.toccane@gmail.com>
2019-09-19 13:23:24 +0200

    add Review routes

--- routes/post.js
+++ routes/post.js
@@ -3,37 +3,37 @@ const router = express.Router();
 
 /* INDEX GET /posts */
 router.get("/", (req, res) => {
-  res.send("INDEX");
+  res.send("Post INDEX");
 });
 
 // NEW /posts/new
 router.get("/new", (req, res) => {
-  res.send("NEW");
+  res.send("Post NEW");
 });
 
 // SHOW GET /posts/:id
 router.get("/:id", (req, res) => {
-  res.send("SHOW");
+  res.send("Post SHOW");
 });
 
 // CREATE POST /posts
 router.post("/", (req, res) => {
-  res.send("CREATE");
+  res.send("Post CREATE");
 });
 
 // EDIT GET /posts/:id/edit
 router.get("/:id/edit", (req, res) => {
-  res.send("EDIT");
+  res.send("Post EDIT");
 });
 
 // UPDATE PUT /posts/:id
 router.put("/:id", (req, res) => {
-  res.send("UPDATE");
+  res.send("Post UPDATE");
 });
 
 // DESTROY DELETE /posts/:id
 router.delete("/:id", (req, res) => {
-  res.send("DESTROY");
+  res.send("Post DESTROY");
 });
 
 module.exports = router;
---
+++ routes/reviews.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+
+/* INDEX GET /posts/:id/reviews */
+router.get("/", (req, res) => {
+  res.send("Review INDEX");
+});
+
+// CREATE POST /posts/:id/reviews
+router.post("/", (req, res) => {
+  res.send("Review CREATE");
+});
+
+// EDIT GET /posts/:id/reviews/:review_id/edit
+router.get("/:review_id/edit", (req, res) => {
+  res.send("Review EDIT");
+});
+
+// UPDATE PUT /posts/:id/reviews/:review_id
+router.put("/:review_id", (req, res) => {
+  res.send("Review UPDATE");
+});
+
+// DESTROY DELETE /posts/:id/reviews/:review_id
+router.delete("/:review_id", (req, res) => {
+  res.send("Review DESTROY");
+});
+
+module.exports = router;
