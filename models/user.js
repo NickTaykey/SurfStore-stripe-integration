839bd12f0bd01493f4944f0b03a9c813743cf5ee
Nick Tk <nico.toccane@gmail.com>
2020-01-31 21:25:04 +0100

    add image field to user model and edit forms to upload image

--- models/user.js
+++ models/user.js
@@ -7,7 +7,10 @@ const UserSchema = new Schema({
   /* L'EMAIL DEVE ESSERE OBBLIGATORIA DA INSERIRE E UNICA PER OGNI UTENTE (se cerchiamo di registrare un
   utente con un email già usata mongoose ci da un errore) */
   email: { type: String, required: true, unique: true },
-  image: String
+  image: {
+    secure_url: { type: String, default: "/images/default-profile.jpg" },
+    public_id: String
+  }
 });
 
 UserSchema.plugin(passportLocalMongoose);
